aae32a9c5e05db4d5dde9c5753b11715931e341b
Quoc Truong <quoct@google.com>
2017-08-02 15:14:04 -0700

    Address PR comment

--- consoledebugger.cc
+++ consoledebugger.cc
@@ -10,8 +10,8 @@
 using namespace std;
 using namespace google_cloud_debugger;
 
-const static std::string quit = "quit";
-
+// This is a simple test app to test out the debugger.
+// This is only for proof of concept.
 int main(int argc, char *argv[]) {
   HRESULT hr;
   string input_string;
@@ -19,14 +19,12 @@ int main(int argc, char *argv[]) {
   string input;
 
   getline(cin, input);
+  // First argument to the test app will be the process ID of the
+  // process we want to debug.
   procId = stoi(argv[1]);
   cout << "input is " << input << " for process ID " << procId;
 
   Debugger debugger;
-
-  if (input.compare(quit) == 0) {
-    return 0;
-  }
 
   hr = debugger.StartDebugging(procId);
   if (FAILED(hr)) {
@@ -34,22 +32,10 @@ int main(int argc, char *argv[]) {
     return -1;
   }
 
+  // This will launch an infinite while loop to wait and read.
+  // TODO(quoct): We may want to do this in a different thread
+  // so that there is a way to quit this test app.
   debugger.SyncBreakpoints();
 
-  /*
-  std::thread sync_thread([](Debugger &debugger) {
-    debugger.SyncBreakpoints();
-  }, debugger);
-
-  while (true) {
-    cin.clear();
-    getline(cin, input);
-    if (input.compare(quit) == 0) {
-      sync_thread.join();
-      break;
-    }
-    std::this_thread::sleep_for(2s);
-  }
-  */
   return 0;
 }
--- launch_vs.ps1
+++
@@ -1,8 +0,0 @@
-$env:CORECLR_PATH = "C:\Users\quoct\Documents\GitHub\coreclr"
-
-$vsPath = "C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\Common7\IDE\devenv.exe"
-#$vsPath = "C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\devenv.exe"
-
-
-$env:CORECLR_ENABLE_PROFILING = 0;
-Start-Process $vsPath
