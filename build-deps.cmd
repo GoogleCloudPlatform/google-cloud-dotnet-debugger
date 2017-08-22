58b3301f2acaea5dc45e9948dfd764f86d5f16e7
GOOGLE\talarico <talarico@google.com>
2017-08-22 12:47:16 -0700

    Changes from PR #43

--- build-deps.cmd
+++ build-deps.cmd
@@ -4,21 +4,10 @@
 :: TODO(talarico): Allow building with multiple Visual Studio versions. 
 
 :: Check that the user passed in a valid parameter.
-if "%1"=="" (
-  echo "First parameter should be the repos root directory"
+if "%3" == "" (
+  echo "Parameters should be: repository root, configuration, platform"
   exit
 )
-
-if "%2"=="" (
-  echo "Second parameter should be the configuration. Debug or Release"
-  exit
-)
-
-if "%3"=="" (
-  echo "Third parameter should be the platform. x86 or x64"
-  exit
-)
-
 
 SET ROOT_DIR=%1
 SET CONFIG=%2
@@ -29,15 +18,18 @@ if "%3" == "x64" (
   SET PLATFORM_STR= Win64
 )
 
-
-:: Clone the extra repository protobuf needs.
-if not exist "%ROOT_DIR%protobuf\gmock" git clone -b release-1.7.0 https://github.com/google/googlemock.git .\protobuf\gmock
-if not exist "%ROOT_DIR%protobuf\gmock\gtest" git clone -b release-1.7.0 https://github.com/google/googletest.git .\protobuf\gmock\gtest
-if not exist "%ROOT_DIR%protobuf\install" mkdir %ROOT_DIR%protobuf\install
-if not exist "%ROOT_DIR%protobuf\cmake\build\solution" mkdir %ROOT_DIR%protobuf\cmake\build\solution
+:: Ensure the submodule are up to date.
+git submodule init
+git submodule update
 
 :: Build the protobuf repository.
 if not exist "%ROOT_DIR%protobuf\%PLATFORM%\%CONFIG%\libprotobuf.lib" (
+  :: Clone the extra repository protobuf needs.
+  if not exist "%ROOT_DIR%protobuf\gmock" git clone -b release-1.7.0 https://github.com/google/googlemock.git %ROOT_DIR%protobuf\gmock
+  if not exist "%ROOT_DIR%protobuf\gmock\gtest" git clone -b release-1.7.0 https://github.com/google/googletest.git %ROOT_DIR%protobuf\gmock\gtest
+  if not exist "%ROOT_DIR%protobuf\install" mkdir %ROOT_DIR%protobuf\install
+  if not exist "%ROOT_DIR%protobuf\cmake\build\solution" mkdir %ROOT_DIR%protobuf\cmake\build\solution
+
   if not exist "%ROOT_DIR%protobuf\%PLATFORM%\%CONFIG%" mkdir %ROOT_DIR%protobuf\%PLATFORM%\%CONFIG%
   cd %ROOT_DIR%protobuf\cmake\build\solution
   cmake -G "Visual Studio 15 2017%PLATFORM_STR%" -DCMAKE_INSTALL_PREFIX=%ROOT_DIR%protobuf %ROOT_DIR%protobuf\cmake
