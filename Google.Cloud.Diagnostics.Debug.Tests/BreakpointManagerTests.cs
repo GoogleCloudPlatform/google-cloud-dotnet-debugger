GoogleCloudPlatform/google-cloud-dotnet-debugger

commit e077b1057e8c753ddfbfe3f69ec4bada922513ee

Refactor the Debugger Agent (#120)
Refactor the Agent into more manageable small classes and add tests for them.

Sorry for the large change here. It's mostly just moving code around but I can try and split this up if you'd like.

Google.Cloud.Diagnostics.Debug/Google.Cloud.Diagnostics.Debug/Agent.cs
@@ -15,14 +15,9 @@
 using Google.Api.Gax;
 using Google.Cloud.Debugger.V2;
 using System;
-using System.Collections.Generic;
-using System.Collections.Concurrent;
 using System.Diagnostics;
-using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
-using StackdriverBreakpoint = Google.Cloud.Debugger.V2.Breakpoint;
-using Grpc.Core;
 
 namespace Google.Cloud.Diagnostics.Debug
 {
@@ -33,27 +28,11 @@ namespace Google.Cloud.Diagnostics.Debug
     /// then register itself with the Stackdriver Debugger API and listen for new breakpoints to watch and
     /// report hit breakpoints.
     /// TODO(talarico): These docs need to be significantly expanded (use watchpoint).
-    /// TODO(talarico): Add example of how to start this.
-    /// TODO(talarico): Refactor this class it's become to random and some functions do too much.
     /// </summary>
     internal sealed class Agent : IDisposable
     {
-        /// <summary>
-        /// The minimum amount of time we will sleep when backing off failed RPC calls.
-        /// The wait time may be higher if the user sets the option.
-        /// </summary>
-        private static readonly TimeSpan _minBackOffWaitTime = TimeSpan.FromSeconds(10);
-
-        /// <summary>
-        /// The maximum amount of time we will sleep when backing off failed RPC calls.
-        /// The wait time may be higher if the user sets the option.
-        /// </summary>
-        private static readonly TimeSpan _maxBackOffWaitTime = TimeSpan.FromSeconds(10);
-
         private readonly AgentOptions _options;
         private readonly DebuggerClient _client;
-        // Key is a location identifier (from path and line number) and value is ID of an existing breakpoint.
-        private readonly ConcurrentDictionary<string, string> _breakpointLocationToId;
         private readonly CancellationTokenSource _cts;
         private readonly TaskCompletionSource<bool> _tcs;
 
@@ -65,8 +44,7 @@ namespace Google.Cloud.Diagnostics.Debug
         public Agent(AgentOptions options, Controller2Client controlClient = null)
         {
             _options = GaxPreconditions.CheckNotNull(options, nameof(options));
-            _client = TryAction(() => new DebuggerClient(options, controlClient));
-            _breakpointLocationToId = new ConcurrentDictionary<string, string>();
+            _client = new DebuggerClient(options, controlClient);
             _cts = new CancellationTokenSource();
             _tcs = new TaskCompletionSource<bool>();
         }
@@ -77,14 +55,14 @@ namespace Google.Cloud.Diagnostics.Debug
         public void StartAndBlock()
         {
             // Register the debuggee.
-            TryAction(() => { _client.Register(); return true; });
+            TryAction(() => _client.Register());
 
+            // Start the debugger.
             ProcessStartInfo startInfo = ProcessUtils.GetStartInfoForInteractiveProcess(
                 _options.Debugger, _options.DebuggerArguments, null);
             _process = Process.Start(startInfo);
 
             // The write server needs to connect first due to initialization logic in the debugger.
-            // TODO(talarico): Is this (^^) true? Should we change this logic?
             StartWriteLoopAsync(_cts.Token).Wait();
             StartReadLoopAsync(_cts.Token).Wait();
 
@@ -101,7 +79,7 @@ namespace Google.Cloud.Diagnostics.Debug
         }
 
         /// <summary>
-        /// Starts a new <see cref="Thread"/>>, will poll the Stackdriver Debugger API for new
+        /// Starts a new <see cref="Thread"/>, will poll the Stackdriver Debugger API for new
         /// breakpoints and reports them to the debugger via a <see cref="NamedPipeServer"/>.
         /// </summary>
         /// <returns>A task representing the asynchronous operation which will be completed when the
@@ -111,66 +89,15 @@ namespace Google.Cloud.Diagnostics.Debug
             TaskCompletionSource<bool> tcs = new TaskCompletionSource<bool>();
             new Thread(() =>
             {
-                using (var server = new BreakpointServer(new NamedPipeServer()))
+                var breakpointServer = new BreakpointServer(new NamedPipeServer());
+                using (var server = new BreakpointWriteActionServer(breakpointServer, _client))
                 {
-                    server.WaitForConnectionAsync().Wait();
-                    tcs.SetResult(true);
-                    RepeatRpcAction(() =>
+                    TryAction(() =>
                     {
-                        var serverBreakpoints = TryAction(() => _client.ListBreakpoints());
-
-                        // Remove no longer active breakpoints from the debugger.
-                        ISet<string> serverBreakpointIdentifiers =
-                            new HashSet<string>(serverBreakpoints.Select(b => b.GetLocationIdentifier()));
-
-                        // Retrieves all the identifiers from the active dictionary that the server no longer has.
-                        IEnumerable<string> breakpointToBeRemovedIdentifiers = _breakpointLocationToId.Keys
-                            .Where(identifier => !serverBreakpointIdentifiers.Contains(identifier));
-                        foreach (string identifierToBeRemoved in breakpointToBeRemovedIdentifiers)
-                        {
-                            string[] locationStrings = identifierToBeRemoved.Split(':');
-                            if (locationStrings.Length != 2)
-                            {
-                                throw new ArgumentException("Location must contains file name and line number.");
-                            }
-
-                            var location = new SourceLocation()
-                            {
-                                Path = locationStrings[0],
-                                Line = Int32.Parse(locationStrings[1])
-                            };
-
-                            var breakpoint = new Breakpoint
-                            {
-                                Id = _breakpointLocationToId[identifierToBeRemoved],
-                                Location = location,
-                                Activated = false,
-                            };
-                            // TryRemove only fails if the key is no longer in the dictionary which
-                            // is what we want anyway.
-                            _breakpointLocationToId.TryRemove(identifierToBeRemoved, out _);
-                            server.WriteBreakpointAsync(breakpoint).Wait();
-                        }
-
-                        // Send all breakpoints to the debugger.
-                        // Even if the breakpoint is already set, the debugger will ignore it
-                        // so we don't have to worry about sending over existing breakpoints.
-                        foreach (StackdriverBreakpoint breakpoint in serverBreakpoints)
-                        {
-                            if (!string.IsNullOrWhiteSpace(breakpoint.Condition) || breakpoint.Expressions.Count() != 0)
-                            {
-                                breakpoint.Status = Common.CreateStatusMessage(
-                                    Messages.CondExpNotSupported, isError: true);
-                                breakpoint.IsFinalState = true;
-                                TryAction(() => _client.UpdateBreakpoint(breakpoint));
-                            }
-                            else
-                            {
-                                _breakpointLocationToId[breakpoint.GetLocationIdentifier()] = breakpoint.Id;
-                                server.WriteBreakpointAsync(breakpoint.Convert()).Wait();
-                            }
-                        }
-                    }, TimeSpan.FromSeconds(_options.WaitTime), cancellationToken);
+                        server.WaitForConnection();
+                        tcs.SetResult(true);
+                        server.StartActionLoop(TimeSpan.FromSeconds(_options.WaitTime), cancellationToken);
+                    });
                 }
             }).Start();
             return tcs.Task;
@@ -187,89 +114,37 @@ namespace Google.Cloud.Diagnostics.Debug
             TaskCompletionSource<bool> tcs = new TaskCompletionSource<bool>();
             new Thread(() =>
             {
-                using (var server = new BreakpointServer(new NamedPipeServer()))
+                var breakpointServer = new BreakpointServer(new NamedPipeServer());
+                using (var server = new BreakpointReadActionServer(breakpointServer, _client))
                 {
-                    server.WaitForConnectionAsync().Wait();
-                    tcs.SetResult(true);
-                    RepeatRpcAction(() =>
-                    {
-                        Task<Breakpoint> breakpointFromDebugger = server.ReadBreakpointAsync();
-                        breakpointFromDebugger.Wait();
-
-                        Breakpoint readBreakpoint = breakpointFromDebugger.Result;
-                        StackdriverBreakpoint breakpoint = readBreakpoint.Convert();
-                        breakpoint.IsFinalState = true;
-
-                        string locationIdentifier = readBreakpoint.GetLocationIdentifier();
-
-                        // Retrieves the appropriate breakpoint id.
-                        if (_breakpointLocationToId.ContainsKey(locationIdentifier))
-                        {
-                            breakpoint.Id = _breakpointLocationToId[locationIdentifier];
-                        }
-                        // Should not throw if the breakpoint is removed from list of active breakpoints.
-                        // User may have removed it before the debugger has a chance to remove the breakpoint.
-
-                        TryAction(() => _client.UpdateBreakpoint(breakpoint));
-                    }, TimeSpan.Zero, cancellationToken);
+                    TryAction(() => 
+                    { 
+                        server.WaitForConnection();
+                        tcs.SetResult(true);
+                        server.StartActionLoop(TimeSpan.Zero, cancellationToken);
+                    });
                 }
             }).Start();
             return tcs.Task;
         }
 
         /// <summary>
-        /// Repeats an action that may throw and <see cref="RpcException"/>.
-        /// If an <see cref="RpcException"/> is thrown the <paramref name="waitTime"/> will double
-        /// until the action is successful (up to <see cref="_maxBackOffWaitTime"/>).  When the action
-        /// is successful the wait between calls will return to the original amount.
-        /// </summary>
-        /// <param name="action">The action to repeated make.</param>
-        /// <param name="waitTime">The time to wait between calls to the action.</param>
-        /// <param name="cancellationToken">A token to signal this action should stop.</param>
-        private void RepeatRpcAction(Action action, TimeSpan waitTime, CancellationToken cancellationToken)
-        {
-            TimeSpan originalWaitTime = waitTime;
-            TimeSpan currentWaitTime = waitTime;
-            while (!cancellationToken.IsCancellationRequested)
-            {
-                try
-                {
-                    action();
-                    currentWaitTime = originalWaitTime;
-                }
-                catch (RpcException e)
-                {
-                    Console.WriteLine( $"RpcException with status code '{e.Status.StatusCode}' \n {e}");
-                    if (currentWaitTime < _maxBackOffWaitTime)
-                    {
-                        currentWaitTime = TimeSpan.FromTicks(currentWaitTime.Ticks * 2);
-                    }
-                    currentWaitTime = currentWaitTime == TimeSpan.Zero ? _minBackOffWaitTime : currentWaitTime;
-                }
-                Thread.Sleep(currentWaitTime);
-            }
-
-        }
-
-        /// <summary>
         /// Tries to perform an action. If a <see cref="DebuggeeDisabledException"/> is
         /// thrown complete the <see cref="_tcs"/> to signal the debugger should be shutdown.
         /// </summary>
-        private T TryAction<T>(Func<T> func)
+        private void TryAction(Action func)
         {
             try
             {
-                return func();
+                 func();
             }
             catch (DebuggeeDisabledException)
             {
                 _tcs.SetResult(true);
-                return default(T);
             }
         }
 
         // TODO(talarico): Move this out of this class.
-        // TODO(talarico): Handle exceptions during startup.
         /// <summary>
         /// Starts a agent and blocks the terminal.
         /// </summary>

commit a07581919869bb47af79e23a93d644ce35f33e8a

Fix a regression in the agent from PR #120 (#130)
* Fix a regression in the agent from #120

This caused breakpoints to no be able to be reset on the same line as breakpoints where no removed when when hit and duplicated in a list call would throw.

I will add an integration tests for this case soon.

* update comment.

* Changes from PR #130

Google.Cloud.Diagnostics.Debug/Google.Cloud.Diagnostics.Debug/Agent.cs
@@ -35,6 +35,7 @@ namespace Google.Cloud.Diagnostics.Debug
         private readonly DebuggerClient _client;
         private readonly CancellationTokenSource _cts;
         private readonly TaskCompletionSource<bool> _tcs;
+        private readonly BreakpointManager _breakpointManager;
 
         private Process _process;
 
@@ -47,6 +48,7 @@ namespace Google.Cloud.Diagnostics.Debug
             _client = new DebuggerClient(options, controlClient);
             _cts = new CancellationTokenSource();
             _tcs = new TaskCompletionSource<bool>();
+            _breakpointManager = new BreakpointManager();
         }
 
         /// <summary>
@@ -89,7 +91,7 @@ namespace Google.Cloud.Diagnostics.Debug
             new Thread(() =>
             {
                 var breakpointServer = new BreakpointServer(new NamedPipeServer());
-                using (var server = new BreakpointWriteActionServer(breakpointServer, _client))
+                using (var server = new BreakpointWriteActionServer(breakpointServer, _client, _breakpointManager))
                 {
                     TryAction(() =>
                     {
@@ -114,7 +116,7 @@ namespace Google.Cloud.Diagnostics.Debug
             new Thread(() =>
             {
                 var breakpointServer = new BreakpointServer(new NamedPipeServer());
-                using (var server = new BreakpointReadActionServer(breakpointServer, _client))
+                using (var server = new BreakpointReadActionServer(breakpointServer, _client, _breakpointManager))
                 {
                     TryAction(() => 
                     { 

Google.Cloud.Diagnostics.Debug/Google.Cloud.Diagnostics.Debug/BreakpointManager.cs
@@ -56,10 +56,21 @@ namespace Google.Cloud.Diagnostics.Debug
         {
             lock (_mutex)
             {
-                var identifiersToBreakpoint = new Dictionary<string, StackdriverBreakpoint>(
-                    activeBreakpoints.ToDictionary(b => b.GetLocationIdentifier(), b => b));
+                // We ignore duplicate breakpoints here as the debugger can only handle one
+                // breakpoint per file and line at a time.  The ignored breakpoint will be
+                // picked up again the next time the breakpoints are polled.
+                // TODO(talarico): We need to revist this logic we when we add conditional
+                // breakpoints or allow breakpoints in columns of lines. 
+                var identifiersToBreakpoint = new Dictionary<string, StackdriverBreakpoint>();
+                foreach (var breakpoint in activeBreakpoints)
+                {
+                    if (!identifiersToBreakpoint.ContainsKey(breakpoint.GetLocationIdentifier()))
+                    {
+                        identifiersToBreakpoint.Add(breakpoint.GetLocationIdentifier(), breakpoint);
+                    }
+                }
 
-                var newBreakpoints = activeBreakpoints.Where(b => !_breakpointDictionary.ContainsKey(b.GetLocationIdentifier())).ToList();
+                var newBreakpoints = identifiersToBreakpoint.Values.Where(b => !_breakpointDictionary.ContainsKey(b.GetLocationIdentifier())).ToList();
                 foreach (var newBreakpoint in newBreakpoints)
                 {
                     _breakpointDictionary[newBreakpoint.GetLocationIdentifier()] = newBreakpoint;
@@ -80,6 +91,33 @@ namespace Google.Cloud.Diagnostics.Debug
                     Removed = removedBreakpoints
                 };
             }
+        }
+
+        /// <summary>
+        /// Remove a breakpoint from the manager.
+        /// </summary>
+        /// <param name="breakpoint">The breakpoint to remove.</param>
+        public void RemoveBreakpoint(StackdriverBreakpoint breakpoint)
+        {
+            lock (_mutex)
+            {
+                _breakpointDictionary.Remove(breakpoint.GetLocationIdentifier());
+            }
+        }
+
+        /// <summary>
+        /// Gets the breakpoint id from the manager based on the breakpoint location.
+        /// If the list does not contain
+        /// Note: This is needed as the debugger does not pay attention to breakpoint ids
+        /// and must be managed here.
+        /// </summary>
+        /// <param name="breakpoint">The breakpoint to get the id of.</param>
+        /// <returns>The breakpoint id or null if none can be found.</returns>
+        public string GetBreakpointId(StackdriverBreakpoint breakpoint)
+        {
+            StackdriverBreakpoint bp;
+            return _breakpointDictionary.TryGetValue(
+                breakpoint.GetLocationIdentifier(), out bp) ? bp.Id : null;
         }
     }
 }

Google.Cloud.Diagnostics.Debug/Google.Cloud.Diagnostics.Debug/BreakpointReadActionServer.cs
@@ -24,16 +24,19 @@ namespace Google.Cloud.Diagnostics.Debug
     public class BreakpointReadActionServer : BreakpointActionServer
     {
         private readonly IDebuggerClient _client;
+        private readonly BreakpointManager _breakpointManager;
 
         /// <summary>
         /// Create a new <see cref="BreakpointReadActionServer"/>.
         /// </summary>
         /// <param name="server">The breakpoint server to communicate with.</param>
         /// <param name="client">The debugger client to send updated breakpoints to.</param>
-        public BreakpointReadActionServer(
-            IBreakpointServer server, IDebuggerClient client) : base (server)
+        /// <param name="breakpointManager">A shared breakpoint manager.</param>
+        public BreakpointReadActionServer(IBreakpointServer server, IDebuggerClient client,
+            BreakpointManager breakpointManager) : base(server)
         {
             _client = GaxPreconditions.CheckNotNull(client, nameof(client));
+            _breakpointManager = GaxPreconditions.CheckNotNull(breakpointManager, nameof(breakpointManager));
         }
 
         /// <summary>
@@ -45,6 +48,8 @@ namespace Google.Cloud.Diagnostics.Debug
             Breakpoint readBreakpoint = _server.ReadBreakpointAsync().Result;
             StackdriverBreakpoint breakpoint = readBreakpoint.Convert();
             breakpoint.IsFinalState = true;
+            breakpoint.Id = _breakpointManager.GetBreakpointId(breakpoint) ?? breakpoint.Id;
+            _breakpointManager.RemoveBreakpoint(breakpoint);
             _client.UpdateBreakpoint(breakpoint);
         }
     }

Google.Cloud.Diagnostics.Debug/Google.Cloud.Diagnostics.Debug/BreakpointWriteActionServer.cs
@@ -32,11 +32,12 @@ namespace Google.Cloud.Diagnostics.Debug
         /// </summary>
         /// <param name="server">The breakpoint server to communicate with.</param>
         /// <param name="client">The debugger client to send updated breakpoints to.</param>
-        public BreakpointWriteActionServer(
-            IBreakpointServer server, IDebuggerClient client) : base (server)
+        /// <param name="breakpointManager">A shared breakpoint manager.</param>
+        public BreakpointWriteActionServer(IBreakpointServer server, IDebuggerClient client, 
+            BreakpointManager breakpointManager) : base (server)
         {
             _client = GaxPreconditions.CheckNotNull(client, nameof(client));
-            _breakpointManager = new BreakpointManager();
+            _breakpointManager = GaxPreconditions.CheckNotNull(breakpointManager, nameof(breakpointManager));
         }
 
         /// <summary>

Google.Cloud.Diagnostics.Debug.Tests/BreakpointManagerTests.cs
@@ -14,6 +14,7 @@
 
 using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.Linq;
 using Xunit;
 
@@ -23,6 +24,8 @@ namespace Google.Cloud.Diagnostics.Debug.Tests
 {
     public class BreakpointManagerTests
     {
+        private static readonly ImmutableList<StackdriverBreakpoint> _emptyList =
+            ImmutableList<StackdriverBreakpoint>.Empty;
         private readonly BreakpointManager _manager;
 
         public BreakpointManagerTests()
@@ -31,7 +34,7 @@ namespace Google.Cloud.Diagnostics.Debug.Tests
         }
 
         [Fact]
-        public void AddNewBreakpoints()
+        public void UpdateBreakpoints_NewBreakpoints()
         {
             var breakpoints = CreateBreakpoints(2);
             var response = _manager.UpdateBreakpoints(breakpoints);
@@ -44,7 +47,19 @@ namespace Google.Cloud.Diagnostics.Debug.Tests
         }
 
         [Fact]
-        public void IgnoreDuplicateBreakpoints()
+        public void UpdateBreakpoints_Duplicate()
+        {
+            var breakpoints = CreateBreakpoints(1).Concat(CreateBreakpoints(1)).ToList();
+            var response = _manager.UpdateBreakpoints(breakpoints);
+
+            Assert.Empty(response.Removed);
+            Assert.Single(response.New);
+
+            Assert.Equal(breakpoints[0], response.New.Single());
+        }
+
+        [Fact]
+        public void UpdateBreakpoints_IgnoreDuplicateBreakpoints()
         {
             var breakpoints = CreateBreakpoints(2);
             var response = _manager.UpdateBreakpoints(breakpoints);
@@ -58,7 +73,7 @@ namespace Google.Cloud.Diagnostics.Debug.Tests
         }
 
         [Fact]
-        public void IgnoreDuplicateBreakpointsBySource()
+        public void UpdateBreakpoints_IgnoreDuplicateBreakpointsBySource()
         {
             var breakpoints = CreateBreakpoints(2);
             var response = _manager.UpdateBreakpoints(breakpoints);
@@ -77,7 +92,7 @@ namespace Google.Cloud.Diagnostics.Debug.Tests
         }
 
         [Fact]
-        public void RemoveBreakpoints()
+        public void UpdateBreakpoints_RemoveBreakpoints()
         {
             var breakpoints = CreateBreakpoints(2);
             _manager.UpdateBreakpoints(breakpoints);
@@ -88,7 +103,7 @@ namespace Google.Cloud.Diagnostics.Debug.Tests
         }
 
         [Fact]
-        public void AddAndRemoveBreakpoints()
+        public void UpdateBreakpoints_AddAndRemoveBreakpoints()
         {
             var breakpoints = CreateBreakpoints(10);
             var response = _manager.UpdateBreakpoints(breakpoints.GetRange(0, 6));
@@ -106,7 +121,47 @@ namespace Google.Cloud.Diagnostics.Debug.Tests
             response = _manager.UpdateBreakpoints(new List<StackdriverBreakpoint>());
             Assert.Empty(response.New);
             Assert.Equal(5, response.Removed.Count());
+        }
 
+        [Fact]
+        public void RemoveBreakpoint()
+        {
+            var breakpoints = CreateBreakpoints(1);
+            _manager.UpdateBreakpoints(breakpoints);
+            _manager.RemoveBreakpoint(breakpoints.Single());
+
+            var response = _manager.UpdateBreakpoints(_emptyList);
+            Assert.Empty(response.New);
+            Assert.Empty(response.Removed);
+        }
+
+        [Fact]
+        public void RemoveBreakpoint_NoEffect()
+        {
+            var breakpoints = CreateBreakpoints(2);
+            _manager.UpdateBreakpoints(breakpoints.GetRange(0, 1));
+            _manager.RemoveBreakpoint(breakpoints[1]);
+
+            var response = _manager.UpdateBreakpoints(_emptyList);
+            Assert.Empty(response.New);
+            Assert.Equal(breakpoints[0], response.Removed.Single());
+        }
+
+        [Fact]
+        public void GetBreakpointId()
+        {
+            var breakpoints = CreateBreakpoints(1);
+            _manager.UpdateBreakpoints(breakpoints);
+            var breakpoint = CreateBreakpoints(1).Single();
+            breakpoint.Id = "not-the-same-id";
+
+            Assert.Equal(breakpoints.Single().Id, _manager.GetBreakpointId(breakpoint));
+        }
+
+        [Fact]
+        public void GetBreakpointId_Null()
+        {
+            Assert.Null(_manager.GetBreakpointId(CreateBreakpoints(1).Single()));
         }
 
         /// <summary>

Google.Cloud.Diagnostics.Debug.Tests/BreakpointReadActionServerTests.cs
@@ -24,13 +24,15 @@ namespace Google.Cloud.Diagnostics.Debug.Tests
         private readonly Mock<IBreakpointServer> _mockBreakpointServer;
         private readonly Mock<IDebuggerClient> _mockDebuggerClient;
         private readonly BreakpointReadActionServer _server;
+        private readonly BreakpointManager _breakpointManager;
 
         public BreakpointReadActionServerTests()
         {
             _mockBreakpointServer = new Mock<IBreakpointServer>();
             _mockDebuggerClient = new Mock<IDebuggerClient>();
+            _breakpointManager = new BreakpointManager();
             _server = new BreakpointReadActionServer(
-                _mockBreakpointServer.Object, _mockDebuggerClient.Object);
+                _mockBreakpointServer.Object, _mockDebuggerClient.Object, _breakpointManager);
         }
 
         [Fact]
@@ -39,6 +41,11 @@ namespace Google.Cloud.Diagnostics.Debug.Tests
             var breakpoint = new Breakpoint
             {
                 Id = "some-id",
+                Location = new SourceLocation
+                {
+                    Line = 1,
+                    Path = "some-path"
+                }
             };
             _mockBreakpointServer.Setup(s => s.ReadBreakpointAsync(It.IsAny<CancellationToken>()))
                 .Returns(Task.FromResult(breakpoint));

Google.Cloud.Diagnostics.Debug.Tests/BreakpointWriteActionServerTests.cs
@@ -29,13 +29,15 @@ namespace Google.Cloud.Diagnostics.Debug.Tests
         private readonly Mock<IBreakpointServer> _mockBreakpointServer;
         private readonly Mock<IDebuggerClient> _mockDebuggerClient;
         private readonly BreakpointWriteActionServer _server;
+        private readonly BreakpointManager _breakpointManager;
 
         public BreakpointWriteActionServerTests()
         {
             _mockBreakpointServer = new Mock<IBreakpointServer>();
             _mockDebuggerClient = new Mock<IDebuggerClient>();
+            _breakpointManager = new BreakpointManager();
             _server = new BreakpointWriteActionServer(
-                _mockBreakpointServer.Object, _mockDebuggerClient.Object);
+                _mockBreakpointServer.Object, _mockDebuggerClient.Object, _breakpointManager);
 
             _mockBreakpointServer.Setup(s => s.WriteBreakpointAsync(
                 It.IsAny<Breakpoint>(), It.IsAny<CancellationToken>()))