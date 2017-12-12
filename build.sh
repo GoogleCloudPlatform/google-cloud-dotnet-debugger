6dd3807c4677ba677803fd973dee202cd15881e5
GOOGLE\talarico <talarico@google.com>
2017-12-12 08:46:45 -0800

    Simplify the build script for linux and update the script to work on windows

--- build.sh
+++ build.sh
@@ -4,17 +4,21 @@
 SCRIPT=$(readlink -f "$0")
 ROOT_DIR=$(dirname "$SCRIPT")
 
-cd $ROOT_DIR/google_cloud_debugger_lib
-make google_cloud_debugger_lib
+if [[ "$OS" == "Windows_NT" ]]
+then
+  if ! $(type devenv &> /dev/null)
+  then
+    error_message="'devenv' is not found. Please install 'devenv' or try "
+    error_message+="running this script from a Developer Command Prompt."
+    echo $error_message
+    exit 1
+  fi
+  devenv $ROOT_DIR/GoogleCloudDebugger.sln //Build
+else
+  make -C $ROOT_DIR/google_cloud_debugger_lib
+  make -C $ROOT_DIR/google_cloud_debugger
+  make -C $ROOT_DIR/google_cloud_debugger_test
+  dotnet build $ROOT_DIR
+fi
 
-cd $ROOT_DIR/google_cloud_debugger
-make google_cloud_debugger
 
-cd $ROOT_DIR/google_cloud_debugger_test
-make google_cloud_debugger_test
-
-dotnet restore $ROOT_DIR/Google.Cloud.Diagnostics.Debug
-dotnet build $ROOT_DIR/Google.Cloud.Diagnostics.Debug
-
-dotnet restore $ROOT_DIR/Google.Cloud.Diagnostics.Debug.Tests
-dotnet build $ROOT_DIR/Google.Cloud.Diagnostics.Debug.Tests
