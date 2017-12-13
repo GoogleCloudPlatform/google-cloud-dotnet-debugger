651d8ecdcad4ebd6d9903db01442504aa7b5af38
Ian <iantalarico@users.noreply.github.com>
2017-12-13 10:33:57 -0800

    Only build submodules when needed on Linux (#178)
    This is already done on Window.
    
    This change also includes updates to travis caching (which should work better with this change) to hopefully reduce build times. We now only cache the built library directories we need. protobuf is not cached as it installs the lib on the system and is a little more complex

--- build-deps.sh
+++ build-deps.sh
@@ -1,29 +1,51 @@
 # The script builds the submodule dependencies of this repository.
-# TODO(talarico): Only rebuild submodules if needed.
 # TODO(talarico): Allow configuration of platform and config
 
 SCRIPT=$(readlink -f "$0")
 ROOT_DIR=$(dirname "$SCRIPT")
 
+GTEST_DIR=$ROOT_DIR/googletest/googletest/make
+GMOCK_DIR=$ROOT_DIR/googletest/googlemock/make
+PROTOBUF_DIR=$ROOT_DIR/protobuf
+CORECLR_DIR=$ROOT_DIR/coreclr
+CORECLR_BIN=$CORECLR_DIR/bin/Product/Linux.x64.Debug
+
 # Build the gtest library
-cd $ROOT_DIR/googletest/googletest/make
-make gtest.a
+if [[ ! -f $GTEST_DIR/gtest.a ]]
+then
+  make -C $GTEST_DIR gtest.a
+else
+  echo "Skipping gtest, it was already built."
+fi
 
 # Build the gmock library
-cd $ROOT_DIR/googletest/googlemock/make
-make gmock.a
+if [[ ! -f $GMOCK_DIR/gmock.a ]]
+then
+  make -C $GMOCK_DIR gmock.a
+else
+  echo "Skipping gmock, it was already built."
+fi
 
 # Build the protobuf library
-cd $ROOT_DIR/protobuf
-./autogen.sh
-./configure
-make
-sudo make install
+if [[ ! $(ldconfig -p | grep libprotobuf.so ) ]]
+then
+  # We need to run in the protobuf directory per comments in autogen.sh
+  cd $PROTOBUF_DIR
+  ./autogen.sh
+  ./configure
+  make
+  sudo make install
+else
+  echo "Skipping protobuf, it was already built."
+fi
 
 # Build the coreclr library
-cd $ROOT_DIR/coreclr
-./build.sh skiptests skipnuget skipmscorlib
+if [[ ! -d $CORECLR_BIN ]]
+then
+  $CORECLR_DIR/build.sh skiptests skipnuget skipmscorlib
+else
+  echo "Skipping coreclr, it was already built."
+fi
 
-# Add the the core clr library path to LD_LIBRARY_PATH
-export LD_LIBRARY_PATH=$ROOT_DIR/coreclr/bin/Product/Linux.x64.Debug
+export LD_LIBRARY_PATH=$CORECLR_BIN
 sudo ldconfig
