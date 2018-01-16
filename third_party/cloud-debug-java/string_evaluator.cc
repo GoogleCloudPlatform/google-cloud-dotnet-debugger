6cc62a688a053926a1706bfe94ae669fed14b772
Quoc Truong <quoct@google.com>
2018-01-16 10:47:27 -0800

    Add string evaluator and error messages file

--- third_party/cloud-debug-java/string_evaluator.cc
+++ third_party/cloud-debug-java/string_evaluator.cc
@@ -15,86 +15,66 @@
  */
 
 #include "string_evaluator.h"
+#include "../../google_cloud_debugger_lib/ccomptr.h"
+#include "../../google_cloud_debugger_lib/i_eval_coordinator.h"
+#include "../../google_cloud_debugger_lib/error_messages.h"
+#include "../../google_cloud_debugger_lib/dbg_object.h"
 
-#include "jni_utils.h"
-#include "messages.h"
-#include "model.h"
+namespace google_cloud_debugger {
 
-namespace devtools {
-namespace cdbg {
-
-// TODO(vlif): refactor this code to use auto-ref classes.
-
-StringEvaluator::StringEvaluator(std::vector<jchar> string_content)
-    : string_content_(std::move(string_content)),
-      jstr_(nullptr) {
+StringEvaluator::StringEvaluator(std::vector<char> string_content)
+    : string_content_(string_content.begin(), string_content.end()) {
 }
 
-
-StringEvaluator::~StringEvaluator() {
-  if (jstr_ != nullptr) {
-    jni()->DeleteGlobalRef(jstr_);
-    jstr_ = nullptr;
-  }
-}
-
-
-bool StringEvaluator::Compile(
-    ReadersFactory* readers_factory,
-    FormatMessageModel* error_message) {
-  DCHECK(jstr_ == nullptr);
-  if (jstr_ != nullptr) {
-    return false;
-  }
-
-  // Calling operator[0] on empty vector is not allowed.
-  const jchar empty[] = { };
-  const jchar* string_ptr = empty;
-  if (!string_content_.empty()) {
-    string_ptr = &string_content_[0];
-  }
-
-  // Create new Java string object (obtaining local reference).
-  // TODO(vlif): wrap with exception checking.
-  jobject jstr_local_ref = jni()->NewString(string_ptr, string_content_.size());
-
-  if (!JniCheckNoException("StringEvaluator::Compile")) {
-    return false;
-  }
-
-  if (jstr_local_ref == nullptr) {
-    LOG(WARNING) << "Java string object could not be create";
-    *error_message = { OutOfMemory };
-    return false;
-  }
-
-  // Convert local reference to a global reference.
-  jstr_ = static_cast<jstring>(jni()->NewGlobalRef(jstr_local_ref));
-  jni()->DeleteLocalRef(jstr_local_ref);
-
-  return true;
-}
-
-
-const JSignature& StringEvaluator::GetStaticType() const {
-  static const JSignature string_signature = {
-    JType::Object,
-    kJavaStringClassSignature
+const TypeSignature& StringEvaluator::GetStaticType() const {
+  static const TypeSignature string_signature = {
+    CorElementType::ELEMENT_TYPE_STRING
   };
 
   return string_signature;
 }
 
 
-ErrorOr<JVariant> StringEvaluator::Evaluate(
-    const EvaluationContext& evaluation_context) const {
-  JVariant result;
-  result.assign_new_ref(JVariant::ReferenceKind::Local, jstr_);
+HRESULT StringEvaluator::Evaluate(
+      std::shared_ptr<google_cloud_debugger::DbgObject> *dbg_object,
+      IEvalCoordinator *eval_coordinator, std::ostream *err_stream) const {
+  if (!dbg_object || !eval_coordinator || !err_stream) {
+    return E_INVALIDARG;
+  }
 
-  return std::move(result);
+  // We use ICorDebugEval to create a string object.
+  CComPtr<ICorDebugEval> debug_eval;
+  HRESULT hr = eval_coordinator->CreateEval(&debug_eval);
+  if (FAILED(hr)) {
+    *err_stream << kFailedEvalCreation;
+    return hr;
+  }
+
+  std::vector<WCHAR> wchar_string = ConvertStringToWCharPtr(string_content_);
+  hr = debug_eval->NewString(wchar_string.data());
+  if (FAILED(hr)) {
+    *err_stream << kFailedToCreateString;
+    return hr;
+  }
+
+  BOOL exception_thrown;
+  CComPtr<ICorDebugValue> debug_string;
+  hr = eval_coordinator->WaitForEval(&exception_thrown, debug_eval, &debug_string);
+  // TODO(quoct): Gets the error from debug_string.
+  if (FAILED(hr)) {
+    *err_stream << kFailedToCreateString;
+    return hr;
+  }
+
+  std::unique_ptr<DbgObject> result_string_obj;
+  hr = DbgObject::CreateDbgObject(debug_string, 2, &result_string_obj, err_stream);
+  if (FAILED(hr)) {
+    *err_stream << kFailedToCreateDbgObject;
+    return hr;
+  }
+
+  *dbg_object = std::move(result_string_obj);
+  return S_OK;
 }
 
-
-}  // namespace cdbg
-}  // namespace devtools
-
+}  // namespace google_cloud_debugger
