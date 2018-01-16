677fc45dcb383058799b3aca496a8cf9aff2593d
Quoc Truong <quoct@google.com>
2018-01-16 10:47:27 -0800

    Add string evaluator and error messages file

--- third_party/cloud-debug-java/string_evaluator.h
+++ third_party/cloud-debug-java/string_evaluator.h
@@ -17,44 +17,42 @@
 #ifndef DEVTOOLS_CDBG_DEBUGLETS_JAVA_STRING_EVALUATOR_H_
 #define DEVTOOLS_CDBG_DEBUGLETS_JAVA_STRING_EVALUATOR_H_
 
+#include <vector>
+
 #include "common.h"
 #include "expression_evaluator.h"
 
-namespace devtools {
-namespace cdbg {
+namespace google_cloud_debugger {
 
-// Evaluates string literal. This involves creation of a new Java string object
-// in "Compile" and returning it in "Compute".
+// Evaluates string literal. This involves creation of a new .NET string object
+// in "Evaluate".
 class StringEvaluator : public ExpressionEvaluator {
  public:
-  explicit StringEvaluator(std::vector<jchar> string_content);
+  explicit StringEvaluator(std::vector<char> string_content);
 
-  ~StringEvaluator() override;
+  // Nothing to do in Compile.
+  HRESULT Compile(
+      DbgStackFrame* readers_factory, std::ostream *err_stream) {
+    return S_OK;
+  };
 
-  bool Compile(
-      ReadersFactory* readers_factory,
-      FormatMessageModel* error_message) override;
+  // Returns CorElementType::String.
+  const TypeSignature& GetStaticType() const override;
 
-  const JSignature& GetStaticType() const override;
-
-  Nullable<jvalue> GetStaticValue() const override { return nullptr; }
-
-  ErrorOr<JVariant> Evaluate(
-      const EvaluationContext& evaluation_context) const override;
+  // Creates a string object in the app domain and uses the references
+  // of that object to create a DbgObject.
+  HRESULT Evaluate(std::shared_ptr<google_cloud_debugger::DbgObject> *dbg_object,
+      IEvalCoordinator *eval_coordinator, std::ostream *err_stream) const override;
 
  private:
-  // Set of Unicode characters definiting the Java string.
-  const std::vector<jchar> string_content_;
-
-  // Global reference to Java string object (product of "Compile").
-  jstring jstr_;
+  // The underlying string content.
+  std::string string_content_;
 
   DISALLOW_COPY_AND_ASSIGN(StringEvaluator);
 };
 
 
-}  // namespace cdbg
-}  // namespace devtools
+}  // namespace google_cloud_debugger
 
 #endif  // DEVTOOLS_CDBG_DEBUGLETS_JAVA_STRING_EVALUATOR_H_
 
