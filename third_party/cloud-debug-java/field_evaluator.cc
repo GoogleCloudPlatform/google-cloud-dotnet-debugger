673817d7cda9428109f75ccb006d0d36b7b07807
Quoc Truong <quoct@google.com>
2018-02-01 15:05:49 -0800

    Add logic to compiler field

--- third_party/cloud-debug-java/field_evaluator.cc
+++ third_party/cloud-debug-java/field_evaluator.cc
@@ -16,303 +16,77 @@
 
 #include "field_evaluator.h"
 
-#include "instance_field_reader.h"
-#include "messages.h"
-#include "model.h"
-#include "readers_factory.h"
-#include "static_field_reader.h"
+#include "compiler_helpers.h"
+#include "dbg_object.h"
+#include "dbg_stack_frame.h"
+#include "error_messages.h"
 
-namespace devtools {
-namespace cdbg {
+namespace google_cloud_debugger {
 
 // Maximum depth of inner classes we support to follow the implicit
 // chain of references.
 static constexpr int kMaxInnerClassesDepth = 10;
 
-std::vector<std::unique_ptr<InstanceFieldReader>>
-CreateInstanceFieldReadersChain(
-    ReadersFactory* readers_factory,
-    const string& class_signature,
-    const string& field_name,
-    FormatMessageModel* error_message) {
-  std::vector<std::unique_ptr<InstanceFieldReader>> chain;
-  std::unique_ptr<InstanceFieldReader> reader;
-
-  // Try as "this.field".
-  reader = readers_factory->CreateInstanceFieldReader(
-      class_signature,
-      field_name,
-      error_message);
-  if (reader != nullptr) {
-    chain.push_back(std::move(reader));
-    return chain;
-  }
-
-  // Inner classes will have "this$N" fields pointing to the outer class.
-  // First level inner class will have "this$0". Second level will have
-  // "this$1" and so on. The instance field can be in either.
-  int inner_depth = kMaxInnerClassesDepth - 1;
-  for (; inner_depth >= 0; --inner_depth) {
-    FormatMessageModel inner_reader_error_message;
-    reader = readers_factory->CreateInstanceFieldReader(
-      class_signature,
-      "this$" + std::to_string(inner_depth),
-      &inner_reader_error_message);
-    if (reader != nullptr) {
-      break;
-    }
-  }
-
-  if (reader == nullptr) {
-    // This is not an inner class or exceeds maximum depth (which is unlikely).
-    return {};
-  }
-
-  // This is an inner class. Iterate through chain of inner classes looking
-  // for references.
-  chain.push_back(std::move(reader));
-
-  for (; inner_depth >= 0; --inner_depth) {
-    reader = readers_factory->CreateInstanceFieldReader(
-        chain.back()->GetStaticType().object_signature,
-        field_name,
-        error_message);
-    if (reader != nullptr) {
-      chain.push_back(std::move(reader));
-      return chain;
-    }
-
-    if (inner_depth > 0) {
-      string this_name = "this$" + std::to_string(inner_depth - 1);
-      reader = readers_factory->CreateInstanceFieldReader(
-          chain.back()->GetStaticType().object_signature,
-          this_name,
-          error_message);
-      if (reader == nullptr) {
-        LOG(WARNING) << "Broken inner classes reference chain, "
-                     << "inner class: "
-                     << chain.back()->GetStaticType().object_signature
-                     << ", field: " << this_name;
-        return {};
-      }
-
-      chain.push_back(std::move(reader));
-    }
-  }
-
-  // We iterated through all the inner classes and still found nothing.
-  return {};
-}
-
-
 FieldEvaluator::FieldEvaluator(
     std::unique_ptr<ExpressionEvaluator> instance_source,
-    string identifier_name,
-    string possible_class_name,
-    string field_name)
+    std::string identifier_name, std::string possible_class_name,
+    std::string field_name)
     : instance_source_(std::move(instance_source)),
       identifier_name_(std::move(identifier_name)),
       possible_class_name_(std::move(possible_class_name)),
-      field_name_(std::move(field_name)),
-      computer_(nullptr) {
-}
+      field_name_(std::move(field_name)) {}
 
-
-FieldEvaluator::~FieldEvaluator() {
-  if (static_field_reader_ != nullptr) {
-    static_field_reader_->ReleaseRef();
-  }
-}
-
-
-bool FieldEvaluator::Compile(
-    ReadersFactory* readers_factory,
-    FormatMessageModel* error_message) {
-  FormatMessageModel instance_field_message;
-  if (CompileInstanceField(readers_factory, &instance_field_message)) {
+HRESULT FieldEvaluator::Compile(DbgStackFrame *stack_frame,
+                                std::ostream *err_stream) {
+  HRESULT hr = CompileInstanceField(stack_frame, err_stream);
+  if (SUCCEEDED(hr)) {
     return true;
   }
 
-  FormatMessageModel static_field_message;
-  if (CompileStaticField(readers_factory, &static_field_message)) {
-    return true;
+  hr = CompileStaticField(stack_frame, err_stream);
+  if (SUCCEEDED(hr)) {
+    return hr;
   }
 
-  const bool specific_instance_field_message =
-      !instance_field_message.format.empty() &&
-      (instance_field_message.format != InvalidIdentifier);
-
-  const bool specific_static_field_message =
-      !static_field_message.format.empty() &&
-      (static_field_message.format != InvalidIdentifier);
-
-  // Choose the specific error.
-  if (specific_instance_field_message) {
-    *error_message = std::move(instance_field_message);
-  } else if (specific_static_field_message) {
-    *error_message = std::move(static_field_message);
-  } else {
-    // Both attempts to compile the expression as an instance field and as a
-    // static field failed with a non specific error. Return the same
-    // unspecific error, just expand the name of identifier to include the
-    // current field name.
-    *error_message = { InvalidIdentifier, { identifier_name_ } };
-  }
-
-  return false;
+  return E_FAIL;
 }
 
-
-bool FieldEvaluator::CompileInstanceField(
-    ReadersFactory* readers_factory,
-    FormatMessageModel* error_message) {
-  if (!instance_source_->Compile(readers_factory, error_message)) {
-    return false;
+HRESULT FieldEvaluator::CompileInstanceField(DbgStackFrame *stack_frame,
+                                             std::ostream *err_stream) {
+  HRESULT hr = instance_source_->Compile(stack_frame, err_stream);
+  if (FAILED(hr)) {
+    return hr;
   }
 
-  const JSignature& instance_source_signature =
+  const TypeSignature &instance_source_signature =
       instance_source_->GetStaticType();
 
-  // Take care of "primitiveArray.length" expression. This is a special
-  // construct in Java language.
-  if ((field_name_ == "length") &&
-      IsArrayObjectType(instance_source_signature)) {
-    result_type_ = { JType::Int };
-    computer_ = &FieldEvaluator::ArrayLengthComputer;
-
-    return true;
-  }
-
-  if (instance_source_signature.type != JType::Object) {
-    *error_message = {
-      PrimitiveTypeField,
-      { TypeNameFromSignature(instance_source_signature), field_name_ }
-    };
-
+  if (instance_source_signature.type_name.empty()) {
+    *err_stream << kTypeNameNotAvailable;
     return false;
   }
 
-  if (instance_source_signature.object_signature.empty()) {
-    LOG(ERROR) << "Signature of source object not available";
-
-    // This should not normally happen, so don't bother with a dedicated error
-    // message template.
-    *error_message = {
-      InstanceFieldNotFound,
-      { TypeNameFromSignature(instance_source_signature), field_name_ }
-    };
-
-    return false;
-  }
-
-  instance_fields_chain_ = CreateInstanceFieldReadersChain(
-      readers_factory,
-      instance_source_signature.object_signature,
-      field_name_,
-      error_message);
-  if (instance_fields_chain_.empty()) {
-    return false;
-  }
-
-  result_type_ = instance_fields_chain_.back()->GetStaticType();
-  computer_ = &FieldEvaluator::InstanceFieldComputer;
-
-  return true;
+  return stack_frame->GetMemberFromClassName(
+      instance_source_signature.type_name, field_name_, &result_type_,
+      &class_property, err_stream);
 }
 
-
-bool FieldEvaluator::CompileStaticField(
-    ReadersFactory* readers_factory,
-    FormatMessageModel* error_message) {
+HRESULT FieldEvaluator::CompileStaticField(DbgStackFrame *stack_frame,
+                                           std::ostream *err_stream) {
   if (possible_class_name_.empty()) {
-    return false;
+    return E_FAIL;
   }
 
-  static_field_reader_ = readers_factory->CreateStaticFieldReader(
-      possible_class_name_, field_name_, error_message);
-  if (static_field_reader_ == nullptr) {
-    return false;
-  }
-
-  result_type_ = static_field_reader_->GetStaticType();
-  computer_ = &FieldEvaluator::StaticFieldComputer;
-
-  return true;
+  return stack_frame->GetMemberFromClassName(possible_class_name_, field_name_,
+                                             &result_type_, &class_property,
+                                             err_stream);
 }
 
-
-ErrorOr<JVariant> FieldEvaluator::Evaluate(
-    const EvaluationContext& evaluation_context) const {
-  return (this->*computer_)(evaluation_context);
+HRESULT FieldEvaluator::Evaluate(std::shared_ptr<DbgObject> *dbg_object,
+                                 IEvalCoordinator *eval_coordinator,
+                                 std::ostream *err_stream) const {
+  // TODO(quoct): IMPLEMENT THIS.
+  return E_NOTIMPL;
 }
 
-
-ErrorOr<JVariant> FieldEvaluator::InstanceFieldComputer(
-    const EvaluationContext& evaluation_context) const {
-  ErrorOr<JVariant> source = instance_source_->Evaluate(evaluation_context);
-  if (source.is_error()) {
-    return source;
-  }
-
-  JVariant result = ErrorOr<JVariant>::detach_value(std::move(source));
-  for (const auto& reader : instance_fields_chain_) {
-    jobject source_jobject = nullptr;
-    if (!result.get<jobject>(&source_jobject)) {
-      return INTERNAL_ERROR_MESSAGE;
-    }
-
-    if (source_jobject == nullptr) {
-      // Attempt to dereference null object.
-      return FormatMessageModel { NullPointerDereference };
-    }
-
-    JVariant next;
-    FormatMessageModel error;
-    if (!reader->ReadValue(source_jobject, &next, &error)) {
-      return error;
-    }
-
-    result = std::move(next);
-  }
-
-  return std::move(result);
-}
-
-
-ErrorOr<JVariant> FieldEvaluator::ArrayLengthComputer(
-    const EvaluationContext& evaluation_context) const {
-  ErrorOr<JVariant> source = instance_source_->Evaluate(evaluation_context);
-  if (source.is_error()) {
-    return source;
-  }
-
-  jobject source_jobject = nullptr;
-  if (!source.value().get<jobject>(&source_jobject)) {
-    return INTERNAL_ERROR_MESSAGE;
-  }
-
-  if (source_jobject == nullptr) {
-    // Attempt to dereference null object.
-    return FormatMessageModel { NullPointerDereference };
-  }
-
-  return JVariant::Int(
-      jni()->GetArrayLength(static_cast<jarray>(source_jobject)));
-}
-
-
-ErrorOr<JVariant> FieldEvaluator::StaticFieldComputer(
-    const EvaluationContext& evaluation_context) const {
-  JVariant result;
-  FormatMessageModel error;
-  if (!static_field_reader_->ReadValue(&result, &error)) {
-    return error;
-  }
-
-  return std::move(result);
-}
-
-
-}  // namespace cdbg
-}  // namespace devtools
-
+}  // namespace google_cloud_debugger
