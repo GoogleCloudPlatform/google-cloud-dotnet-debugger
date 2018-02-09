feb0fff75e59ef14952710ff358322d82f0da151
Ian Talarico <talarico@google.com>
2018-02-09 14:01:44 +0000

    Fix the package script

--- package.sh
+++ package.sh
@@ -26,12 +26,12 @@ mkdir -p $TEMP_AGENT_DIR
 mkdir -p $TEMP_DEBUGGER_DIR
 
 # Publish the agent.
-dotnet restore -r debian.8-x64 
+dotnet restore -r debian.8-x64 $AGENT_DIR 
 dotnet publish -c Debug -f netcoreapp2.0 -r debian.8-x64 $AGENT_DIR/Google.Cloud.Diagnostics.Debug/Google.Cloud.Diagnostics.Debug.csproj
 cp -r $AGENT_DIR/Google.Cloud.Diagnostics.Debug/bin/Debug/netcoreapp2.0/debian.8-x64/publish/* $TEMP_AGENT_DIR
 
 # Copy over the debugger.
-cp $DEBUGGER_DIR/google_cloud_debugger $TEMP_DEBUGGER_DIR
+cp $DEBUGGER_DIR/google_cloud_debugger/google_cloud_debugger $TEMP_DEBUGGER_DIR
 
 # Copy the needed so files.
 cp $THIRD_PARTY_DIR/protobuf/src/.libs/libprotobuf.so.13.0.2 $TEMP_DEBUGGER_DIR/libprotobuf.so.13
