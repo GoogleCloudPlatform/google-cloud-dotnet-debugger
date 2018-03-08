e754391f9754ac710fc1c6b50209575f5a743b10
Quoc Truong <quoct@google.com>
2018-03-08 15:09:42 -0800

    Fix formatting and comment

--- src/google_cloud_debugger/google_cloud_debugger_lib/breakpoint_location_collection.h
+++ src/google_cloud_debugger/google_cloud_debugger_lib/breakpoint_location_collection.h
@@ -19,9 +19,9 @@
 #include <mutex>
 #include <vector>
 
+#include "ccomptr.h"
 #include "cor.h"
 #include "cordebug.h"
-#include "ccomptr.h"
 #include "dbg_breakpoint.h"
 
 namespace google_cloud_debugger {
@@ -29,54 +29,57 @@ namespace google_cloud_debugger {
 // Class for managing a collection of breakpoints at the same location.
 class BreakpointLocationCollection {
  public:
-   // Returns a vector containing all the breakpoints at this location.
-   std::vector<std::shared_ptr<DbgBreakpoint>> GetBreakpoints();
+  // Returns a vector containing all the breakpoints at this location.
+  std::vector<std::shared_ptr<DbgBreakpoint>> GetBreakpoints();
 
-   // Add the first breakpoint at this location to this collection.
-   // This method should be called to populate the cache.
-   HRESULT AddFirstBreakpoint(std::shared_ptr<DbgBreakpoint> breakpoint);
+  // Add the first breakpoint at this location to this collection.
+  // This method should be called to populate the cache.
+  // TODO(quoct): Refactor this to make this call private and merge
+  // the logic into UpdateBreakpoints.
+  HRESULT AddFirstBreakpoint(std::shared_ptr<DbgBreakpoint> breakpoint);
 
-   // If breakpoint breakpoint exists in breakpoints_ with the same ID,
-   // this method will:
-   //  1. Activate ICorDebugBreakpoint at this location if breakpoint is
-   // active and ICorDebugBreakpoint is inactive.
-   //  2. Deactivate ICorDebugBreakpoint at this location if breakpoint is
-   // inactive and ICorDebugBreakpoint is active and ALL breakpoints
-   // at this location are inactive.
-   // If there are no breakpoints with the same ID in breakpoints_,
-   // this method will create a new breakpoint based on breakpoint.
-   // This new breakpoint will have its IL Offset, method def, method token,
-   // etc. set from the cache of this BreakpointLocationCollection.
-   // The method will then activate or deactivate ICorDebugBreakpoint according
-   // to the logic above.
-   HRESULT UpdateBreakpoints(const DbgBreakpoint &breakpoint);
+  // If there are no breakpoints
+  // If breakpoint breakpoint exists in breakpoints_ with the same ID,
+  // this method will:
+  //  1. Activate ICorDebugBreakpoint at this location if breakpoint is
+  // active and ICorDebugBreakpoint is inactive.
+  //  2. Deactivate ICorDebugBreakpoint at this location if breakpoint is
+  // inactive and ICorDebugBreakpoint is active and ALL breakpoints
+  // at this location are inactive.
+  // If there are no breakpoints with the same ID in breakpoints_,
+  // this method will create a new breakpoint based on breakpoint.
+  // This new breakpoint will have its IL Offset, method def, method token,
+  // etc. set from the cache of this BreakpointLocationCollection.
+  // The method will then activate or deactivate ICorDebugBreakpoint according
+  // to the logic above.
+  HRESULT UpdateBreakpoints(const DbgBreakpoint &breakpoint);
 
-   // Returns the IL Offset of breakpoints at this location.
-   uint32_t GetILOffset() { return il_offset_; }
+  // Returns the IL Offset of breakpoints at this location.
+  uint32_t GetILOffset() { return il_offset_; }
 
-   // Returns the method token of breakpoints at this location.
-   mdMethodDef GetMethodToken() { return method_token_; }
+  // Returns the method token of breakpoints at this location.
+  mdMethodDef GetMethodToken() { return method_token_; }
 
  private:
-   // Mutex to protect breakpoints_ vector from mulutiple access.
+  // Mutex to protect breakpoints_ vector from mulutiple access.
   std::mutex mutex_;
-   
-   // Helper function to update an existing breakpoint in breakpoints_ that
-    // has the same ID as that of an existing breakpoint.
-    HRESULT UpdateExistingBreakpoint(const DbgBreakpoint &breakpoint);
 
-   // Activate the ICorDebugBreakpoint at this location.
-   // Provide FALSE to deactivate the breakpoint.
-   // Deactivation will only succeed if none of the existing breakpoints
-   // are in an active state.
-   HRESULT ActivateCorDebugBreakpointHelper(BOOL activation_state);
+  // Helper function to update an existing breakpoint in breakpoints_ that
+  // has the same ID as that of an existing breakpoint.
+  HRESULT UpdateExistingBreakpoint(const DbgBreakpoint &breakpoint);
 
-   // Collection of breakpoints at this location.
-   std::vector<std::shared_ptr<DbgBreakpoint>> breakpoints_;
+  // Activate the ICorDebugBreakpoint at this location.
+  // Provide FALSE to deactivate the breakpoint.
+  // Deactivation will only succeed if none of the existing breakpoints
+  // are in an active state.
+  HRESULT ActivateCorDebugBreakpointHelper(BOOL activation_state);
 
-   // ICorDebugBreakpoint that represents the breakpoints at this location.
-   // We only need 1.
-   CComPtr<ICorDebugBreakpoint> cor_debug_breakpoint_;
+  // Collection of breakpoints at this location.
+  std::vector<std::shared_ptr<DbgBreakpoint>> breakpoints_;
+
+  // ICorDebugBreakpoint that represents the breakpoints at this location.
+  // We only need 1.
+  CComPtr<ICorDebugBreakpoint> cor_debug_breakpoint_;
 
   // The IL Offset of breakpoints at this location.
   uint32_t il_offset_;
@@ -90,11 +93,12 @@ class BreakpointLocationCollection {
   // The name of the method of breakpoints at this location.
   std::vector<WCHAR> method_name_;
 
-  // The ICorDebugBreakpoint that corresponds with breakpoint set at this location.
+  // The ICorDebugBreakpoint that corresponds with breakpoint set at this
+  // location.
   CComPtr<ICorDebugBreakpoint> debug_breakpoint_;
 
-   // String that represents the location.
-   std::string location_string_;
+  // String that represents the location.
+  std::string location_string_;
 };
 
 }  // namespace google_cloud_debugger
