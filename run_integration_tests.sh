4f27abc9d5f245f4dca6c790bc1b07b50f0bba33
GOOGLE\talarico <talarico@google.com>
2018-05-16 15:53:58 -0700

    Add more flixibility to run all types of integration and performance tests to the run_integration_tests.sh script.

--- run_integration_tests.sh
+++ run_integration_tests.sh
@@ -10,14 +10,21 @@ ROOT_DIR=$(dirname "$SCRIPT")
 
 AGENT_DIR=$ROOT_DIR/src/Google.Cloud.Diagnostics.Debug
 
-cd $AGENT_DIR
-
 PERFORMANCE_TESTS=false
+LONG_RUNNING_TESTS=false
+INTEGRATION_TESTS=true
 CONFIG=Debug
+
 while (( "$#" )); do
   if [[ "$1" == "--performance-tests" ]]
   then 
     PERFORMANCE_TESTS=true
+  elif [[ "$1" == "--skip-integration-tests" ]]
+  then
+    INTEGRATION_TESTS=false
+  elif [[ "$1" == "--long-running-tests" ]]
+  then
+    LONG_RUNNING_TESTS=true
   elif [[ "$1" == "--release" ]]
   then
     CONFIG=Release
@@ -25,13 +32,25 @@ while (( "$#" )); do
   shift
 done
 
-export LD_LIBRARY_PATH=$ROOT_DIR/third_party/coreclr/bin/Product/Linux.x64.$CONFIG
+if [[ "$OS" != "Windows_NT" ]]
+then
+  export LD_LIBRARY_PATH=$ROOT_DIR/third_party/coreclr/bin/Product/Linux.x64.$CONFIG
+fi
 
 dotnet publish $AGENT_DIR/Google.Cloud.Diagnostics.Debug --configuration $CONFIG
 dotnet publish $AGENT_DIR/Google.Cloud.Diagnostics.Debug.TestApp --configuration $CONFIG
-dotnet test $AGENT_DIR/Google.Cloud.Diagnostics.Debug.IntegrationTests --configuration $CONFIG
+
+if [[ "$INTEGRATION_TESTS" == true ]]
+then
+  dotnet test $AGENT_DIR/Google.Cloud.Diagnostics.Debug.IntegrationTests --configuration $CONFIG
+fi
 
 if [[ "$PERFORMANCE_TESTS" == true ]]
 then
   dotnet test $AGENT_DIR/Google.Cloud.Diagnostics.Debug.PerformanceTests --configuration $CONFIG
+fi
+
+if [[ "$LONG_RUNNING_TESTS" == true ]]
+then
+  dotnet test $AGENT_DIR/Google.Cloud.Diagnostics.Debug.LongRunningTests --configuration $CONFIG
 fi
