6b428555b219382c78fade1d4478f5ae59a5ee3d
GOOGLE\talarico <talarico@google.com>
2018-05-23 13:31:24 -0700

    Update our packaging scripts to use ubuntu.16.04-x64
    This is currently what our flex images use and we should follow what they do.

--- package.sh
+++ package.sh
@@ -32,8 +32,8 @@ $ROOT_DIR/run_integration_tests.sh --release
 
 # Publish the agent.
 dotnet restore -r debian.8-x64 $AGENT_DIR 
-dotnet publish -c Release -f netcoreapp2.0 -r debian.8-x64 $AGENT_DIR/Google.Cloud.Diagnostics.Debug/Google.Cloud.Diagnostics.Debug.csproj
-cp -r $AGENT_DIR/Google.Cloud.Diagnostics.Debug/bin/Release/netcoreapp2.0/debian.8-x64/publish/* $TEMP_AGENT_DIR
+dotnet publish -c Release -f netcoreapp2.0 -r ubuntu.16.04-x64 $AGENT_DIR/Google.Cloud.Diagnostics.Debug/Google.Cloud.Diagnostics.Debug.csproj
+cp -r $AGENT_DIR/Google.Cloud.Diagnostics.Debug/bin/Release/netcoreapp2.0/ubuntu.16.04-x64/publish/* $TEMP_AGENT_DIR
 
 # Copy over the debugger.
 cp $DEBUGGER_DIR/google_cloud_debugger/google_cloud_debugger $TEMP_DEBUGGER_DIR
