bc850aae01732135adc013208682eb52b1477fb4
Quoc Truong <quoct@google.com>
2018-06-01 11:16:27 -0700

    Add ability to process constant enum in DbgEnum

--- src/google_cloud_debugger/google_cloud_debugger_lib/dbg_enum.h
+++ src/google_cloud_debugger/google_cloud_debugger_lib/dbg_enum.h
@@ -15,7 +15,9 @@
 #ifndef DBG_ENUM_H_
 #define DBG_ENUM_H_
 
+#include <map>
 #include <memory>
+#include <tuple>
 #include <vector>
 
 #include "dbg_class.h"
@@ -26,10 +28,35 @@ namespace google_cloud_debugger {
 // (CorElementType as ELEMENT_TYPE_CLASS and ELEMENT_TYPE_VALUETYPE).
 class DbgEnum : public DbgClass {
  public:
-  DbgEnum(ICorDebugType *debug_type, int depth,
+  // This constructor should be used we have an ICorDebugType
+  // that represents the type of the enum.
+  DbgEnum(ICorDebugType *debug_type, int depth, const std::string &enum_name,
+          mdTypeDef enum_token, std::shared_ptr<ICorDebugHelper> debug_helper,
+          std::shared_ptr<IDbgObjectFactory> obj_factory)
+      : DbgClass(debug_type, depth, debug_helper, obj_factory) {
+    class_name_ = enum_name;
+    class_type_ = ClassType::ENUM;
+    cor_element_type_ = CorElementType::ELEMENT_TYPE_VALUETYPE;
+    class_token_ = enum_token;
+  }
+
+  // This constructor should be used when we do not have an ICorDebugType
+  // that represents the type of the enum.
+  // This will sets the underlying numerical value of the enum directly.
+  // This constructor is used for creating enum from constant literal
+  // as we don't have an ICorDebugType generated for those.
+  DbgEnum(int depth, const std::string &enum_name, mdTypeDef enum_token,
+          ULONG64 enum_value, const CorElementType &enum_type,
           std::shared_ptr<ICorDebugHelper> debug_helper,
           std::shared_ptr<IDbgObjectFactory> obj_factory)
-      : DbgClass(debug_type, depth, debug_helper, obj_factory) {}
+      : DbgClass(nullptr, depth, debug_helper, obj_factory) {
+    class_name_ = enum_name;
+    class_type_ = ClassType::ENUM;
+    cor_element_type_ = CorElementType::ELEMENT_TYPE_VALUETYPE;
+    class_token_ = enum_token;
+    enum_value_ = enum_value;
+    enum_type_ = enum_type;
+  }
 
   // Populate variable with the value of the enum.
   HRESULT PopulateValue(
@@ -37,10 +64,31 @@ class DbgEnum : public DbgClass {
 
   // Extracts out the enum value of this Enum class and processes
   // the fields of this enum.
-  HRESULT ProcessEnum(ICorDebugValue *debug_value, ICorDebugClass *debug_class,
+  HRESULT ProcessEnum(ICorDebugValue *debug_value,
                       IMetaDataImport *metadata_import);
 
+  // Processes the fields of this enum.
+  HRESULT ProcessEnumFields(IMetaDataImport *metadata_import);
+
+  // Sets the underlying integral value of the enum.
+  void SetEnumValue(ULONG64 enum_value) { enum_value_ = enum_value; }
+
+  // Sets the underlying enum type.
+  void SetEnumType(const CorElementType &enum_type) { enum_type_ = enum_type; }
+
  private:
+  // Assuming the fields of this enum has been processed,
+  // add their values to the enum_values_dict.
+  void AddEnumValuesToDict();
+
+  // Key in the dictionary is the name of the enum.
+  // Values are the list of possible values of that enum.
+  // For example, enum Week { Monday, Tuesday } will have key
+  // as Week and values as (0, "Monday") and (1, "Tuesday").
+  static std::map<std::string,
+                  std::vector<std::tuple<UVCP_CONSTANT, std::string>>>
+      enum_values_dict;
+
   // Given a void pointer and type of the enum, extract out the enum
   // value.
   ULONG64 ExtractEnumValue(CorElementType enum_type, void *enum_value);
@@ -52,8 +100,14 @@ class DbgEnum : public DbgClass {
   // the value of an enum.
   static const std::string kEnumValue;
 
+  // The integral type of the enum.
+  CorElementType enum_type_ = CorElementType::ELEMENT_TYPE_END;
+
   // Stores the enum value as a string.
   std::string enum_string_;
+
+  // The underlying integral value of the enum.
+  ULONG64 enum_value_;
 };
 
 }  //  namespace google_cloud_debugger
