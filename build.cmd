404a5014400f092642c2d202c9c8aad14d612edd
GOOGLE\talarico <talarico@google.com>
2018-06-07 09:29:42 -0700

    try build cmd to debug

---
+++ build.cmd
@@ -0,0 +1,15 @@
+
+SET ROOT_DIR=%~dp0
+SET CONFIG=%1
+
+
+SET AGENT_DIR=%ROOT_DIR%\src\Google.Cloud.Diagnostics.Debug
+SET DEBUGGER_DIR=%ROOT_DIR%\src\google_cloud_debugger
+
+if "%1" == "" (
+  SET CONFIG=Debug
+)
+
+dotnet build %AGENT_DIR% --configuration %CONFIG%
+
+msbuild %DEBUGGER_DIR%\google_cloud_debugger.sln /p:Configuration=%CONFIG% /p:Platform=x64
