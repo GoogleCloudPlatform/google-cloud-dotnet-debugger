9d62b60f5b40b367e99802f2339f83eea8d11d05
Ian <iantalarico@users.noreply.github.com>
2018-07-26 16:59:34 -0700

    Add Kokoro directory with helper scripts to enable the CI/CD (#356)
    These will allow us to keep the logic for the CI/CD in source control and make running the CI/CD easier.

---
+++ .kokoro/run_integration_tests.bat
@@ -0,0 +1,11 @@
+:: See documentation in type-shell-output.bat
+
+cd %~dp0
+cd ..
+
+set GOOGLE_APPLICATION_CREDENTIALS="$KOKORO_KEYSTORE_DIR\73609_cloud-sharp-jenkins-compute-service-account"
+
+call .\build-deps.cmd
+call .\build.cmd
+
+call %~dp0type-shell-output.bat run_integration_tests
---
+++ .kokoro/run_unit_tests.bat
@@ -0,0 +1,9 @@
+:: See documentation in type-shell-output.bat
+
+cd %~dp0
+cd ..
+
+call .\build-deps.cmd
+call .\build.cmd
+
+call %~dp0type-shell-output.bat run_unit_tests
