c69edea582c1321cacf75933d08ce06c25c02cbb
Ian <iantalarico@users.noreply.github.com>
2018-07-31 08:55:16 -0700

    Ensure the LD_LIBRARY_PATH is set when running on Kokoro

--- .kokoro/run_integration_tests.sh
+++ .kokoro/run_integration_tests.sh
@@ -4,16 +4,19 @@ set -e
 
 SCRIPT=$(readlink -f "$0")
 SCRIPT_DIR=$(dirname "$SCRIPT")
+ROOT_DIR=$(dirname "$SCRIPT_DIR")
 
 cd $SCRIPT_DIR
 cd ..
 
 export GOOGLE_APPLICATION_CREDENTIALS="$KOKORO_KEYSTORE_DIR/73609_cloud-sharp-jenkins-compute-service-account"
 
-sudo ldconfig
 git submodule init
 git submodule update
 
 ./build-deps.sh
+export LD_LIBRARY_PATH=$ROOT_DIR/third_party/coreclr/bin/Product/Linux.x64.Debug
+sudo ldconfig
+
 ./build.sh
 ./run_integration_tests.sh
