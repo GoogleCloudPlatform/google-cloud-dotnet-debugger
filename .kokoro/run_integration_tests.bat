5b231fb6edaca6995ef24a689e14073e0ac3f2a9
GOOGLE\talarico <talarico@google.com>
2018-08-02 11:01:40 -0700

    update

--- .kokoro/run_integration_tests.bat
+++ .kokoro/run_integration_tests.bat
@@ -8,7 +8,6 @@ set GOOGLE_APPLICATION_CREDENTIALS="$KOKORO_KEYSTORE_DIR\73609_cloud-sharp-jenki
 git submodule init
 git submodule update
 
-call /k .\build-deps.cmd
-call /k .\build.cmd
-
+.\build-deps.cmd & ^
+.\build.cmd & ^
 "C:\Program Files\Git\bin\bash.exe" run_integration_tests.sh
--- .kokoro/run_unit_tests.bat
+++ .kokoro/run_unit_tests.bat
@@ -6,7 +6,6 @@ cd ..
 git submodule init
 git submodule update
 
-call /k .\build-deps.cmd
-call /k .\build.cmd
-
+.\build-deps.cmd & ^
+.\build.cmd & ^
 "C:\Program Files\Git\bin\bash.exe" run_unit_tests.sh
